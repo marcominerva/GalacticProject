763397dc503d17f008a8a050f58cdfdd2ed41c97
Marco Minerva <marco.minerva@gmail.com>
2021-01-30 17:36:38 +0100

    Fix property type

--- AwesomeBackend/AwesomeBackend.Shared/Models/Responses/Restaurant.cs
+++ AwesomeBackend/AwesomeBackend.Shared/Models/Responses/Restaurant.cs
@@ -18,7 +18,7 @@ namespace AwesomeBackend.Shared.Models.Responses
 
         public string WebSite { get; set; }
 
-        public int? RatingsCount { get; set; }
+        public int RatingsCount { get; set; }
 
         public double? RatingScore { get; set; }
     }
